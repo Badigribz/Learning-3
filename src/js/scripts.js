28a0ccdf1772ee4166e9c099fb0837b56505c71d
Gribz <gribzbadi@gmail.com>
2025-10-30 18:41:29 +0300

    added the parcel installation which added parcel cache/dist/node modules folder  also moded package.json/index.html and script.js

--- src/js/scripts.js
+++ src/js/scripts.js
@@ -1,10 +1,12 @@
+//where 3d happens
 import * as THREE from 'three';
 // import { GLTFLoader } from 'three/examples/jsm/loaders/GLTFLoader.js';
 //import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls.js';
 
 const renderer = new THREE.WebGLRenderer();
 
-//defining size of the renderer
+//defining size of the space on page
 renderer.setSize(window.innerWidth, window.innerHeight);
 
+//injecting the 3d space into the body of the html
 document.body.appendChild(renderer.domElement);
